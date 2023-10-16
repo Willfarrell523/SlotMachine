d85907828fc20b626cd47e619dd40c750f82fdc1
William Farrell <willfarrell22@gmail.com>
2023-10-15 21:43:47 -0500

    Removed double counting of paylines, no simulatenous symbol line pays

--- main.py
+++ main.py
@@ -38,54 +38,79 @@ def play():
     # Define a few pay lines using (row, column) coordinates
     pay_lines = {
         # Straght across
-        '1': [[(0, 0), (0, 1), (0, 2)], 1],  # line 1
-        '2': [[(1, 0), (1, 1), (1, 2)], 1],  # line 2
-        '3': [[(2, 0), (2, 1), (2, 2)], 1], # line 3
-        '4': [[(3, 0), (3, 1), (3, 2)], 1], # line 4
-        '5': [[(4, 0), (4, 1), (4, 2)], 1], # line 5
+        '1': [[(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)], 3],  # line 11
+        '2': [[(1, 0), (1, 1), (1, 2), (1, 3), (1, 4)], 3],  # line 12
+        '3': [[(2, 0), (2, 1), (2, 2), (2, 3), (2, 4)], 3],  # line 13
+        '4': [[(3, 0), (3, 1), (3, 2), (3, 3), (3, 4)], 3],  # line 14
+        '5': [[(4, 0), (4, 1), (4, 2), (4, 3), (4, 4)], 3],  # line 15
 
-        '6': [[(0, 0), (0, 1), (0, 2), (0, 3)], 2], # line 6
-        '7': [[(1, 0), (1, 1), (1, 2), (1, 3)], 2], # line 7
-        '8': [[(2, 0), (2, 1), (2, 2), (2, 3)], 2], # line 8 
-        '9': [[(3, 0), (3, 1), (3, 2), (3, 3)], 2], # line 9
-        '10': [[(4, 0), (4, 1), (4, 2), (4, 3)], 2], # line 10 
+        '6': [[(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)], 3],  # line 20 diags
+        '7': [[(4, 0), (3, 1), (2, 2), (1, 3), (0, 4)], 3],  # line 21
 
-        '11': [[(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)], 3],  # line 
-        '12': [[(1, 0), (1, 1), (1, 2), (1, 3), (1, 4)], 3],  # line 
-        '13': [[(2, 0), (2, 1), (2, 2), (2, 3), (2, 4)], 3],  # line 
-        '14': [[(3, 0), (3, 1), (3, 2), (3, 3), (3, 4)], 3],  # line 
-        '15': [[(4, 0), (4, 1), (4, 2), (4, 3), (4, 4)], 3],  # line 
-
-        # Diagonals
-        '16': [[(0, 0), (1, 1), (2, 2)], 1],  # line 
-        '17': [[(2, 0), (1, 1), (0, 2)], 1],  # line 
+        '8': [[(0, 0), (0, 1), (0, 2), (0, 3)], 2], # line 6
+        '9': [[(1, 0), (1, 1), (1, 2), (1, 3)], 2], # line 7
+        '10': [[(2, 0), (2, 1), (2, 2), (2, 3)], 2], # line 8 
+        '11': [[(3, 0), (3, 1), (3, 2), (3, 3)], 2], # line 9
+        '12': [[(4, 0), (4, 1), (4, 2), (4, 3)], 2], # line 10 
         
-        '18': [[(0, 0), (1, 1), (2, 2), (3, 3)], 2],
-        '19': [[(4, 0), (3, 1), (2, 2), (1, 3)], 2],
+        
+        '13': [[(0, 0), (1, 1), (2, 2), (3, 3)], 2], # line 18 diags
+        '14': [[(4, 0), (3, 1), (2, 2), (1, 3)], 2], # line 19 
 
-        '20': [[(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)], 3],  # line 
-        '21': [[(4, 0), (3, 1), (2, 2), (1, 3), (0, 4)], 3],  # line 
-    
+        
+        '15': [[(0, 0), (1, 1), (2, 2)], 1],  # line 16
+        '16': [[(2, 0), (1, 1), (0, 2)], 1],  # line 17
+
+        '17': [[(0, 0), (0, 1), (0, 2)], 1], # line 1
+        '18': [[(1, 0), (1, 1), (1, 2)], 1], # line 2
+        '19': [[(2, 0), (2, 1), (2, 2)], 1], # line 3
+        '20': [[(3, 0), (3, 1), (3, 2)], 1], # line 4
+        '21': [[(4, 0), (4, 1), (4, 2)], 1], # line 5
+
+        
+
     }
+    paid_lines = []
 
-    def calc_pay(symbol, count):
-        
-        paid = char_pay[symbol] * count
+    def calc_pay(symbol, multiplier):
+        paid = char_pay[symbol] * multiplier
         return paid
 
     def check_payout(grid):
-        
+        lineNum = 0
         paid = 0
-        for lineNum, line_data in pay_lines.items():
-            pay_line, multiplier = line_data
+        counted_symbols = set()  # Track symbols that have been counted
+        counted_coordinates = set()  # Track coordinates that have been counted
+
+        for pay_line_key in pay_lines:
+            pay_line = pay_lines[pay_line_key][0]
+            lineNum += 1
+
+            # Check if any coordinate in this pay line has already been counted
+            if any(coord in counted_coordinates for coord in pay_line):
+                continue
+
             symbols_on_line = [grid[row][col] for (row, col) in pay_line]
+
             if len(set(symbols_on_line)) == 1:  # All symbols on the line are the same
-                linepay = calc_pay(symbols_on_line[0], multiplier)
-                paid = paid + linepay
+                symbol = symbols_on_line[0]
+
+                # Check if this symbol has already been counted for a longer line
+                if symbol in counted_symbols:
+                    continue
+
+                linepay = calc_pay(symbol, len(pay_line))
+                paid += linepay
                 print(
-                    f"Payout! You got a winning line with the symbol {symbols_on_line[0]} with line {lineNum}. Pays ${linepay}"
+                    f"Payout! You got a winning line with the symbol {symbol} with line {lineNum}. Pays ${linepay}"
                 )
+
+                # Remember this symbol and its coordinates so we don't double-count
+                counted_symbols.add(symbol)
+                counted_coordinates.update(pay_line)
+
         print(f"Your total winnings are ${paid}")
+
 
   
   
